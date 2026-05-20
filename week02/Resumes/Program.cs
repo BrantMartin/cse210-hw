01a951f66293c59a5757fd185b5307dc6d5ee5ea
Brant Martin <100722069+BrantMartin@users.noreply.github.com>
2026-05-20 01:24:40 -0700

    abstraction

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job micJob = new Job();
+        micJob._jobTitle = "Software Engineer";
+        micJob._company = "Microsoft";
+        micJob._startYear = 2019;
+        micJob._endYear = 2022;
+
+        Job appJob = new Job();
+        appJob._jobTitle = "Manager";
+        appJob._company = "Apple";
+        appJob._startYear = 2022;
+        appJob._endYear = 2023;
+
+        Resume myResume = new Resume();
+        myResume._name = "Allison Rose";
+
+        myResume._jobs.Add(micJob);
+        myResume._jobs.Add(appJob);
+
+        myResume.Display();
     }
 }
