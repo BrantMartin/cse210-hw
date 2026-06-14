89570e590c4ea4cb5fbf718e4d5ae1306d20e2ee
Brant Martin <100722069+BrantMartin@users.noreply.github.com>
2026-06-14 14:14:18 -0700

    shapes week 6

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(new Square(3.0));
+        shapes[0].SetColor("Red");
+        shapes.Add(new Rectangle(3.0, 4.0));
+        shapes[1].SetColor("Blue");
+        shapes.Add(new Circle(3.0));
+        shapes[2].SetColor("Yellow");
+
+        for (int i = 0; i < shapes.Count(); i++)
+        {
+            Console.WriteLine(shapes[i].GetColor());
+            Console.WriteLine(shapes[i].GetArea());
+        }
     }
 }
